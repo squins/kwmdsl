5a43f9c4b620502f9066718e06bd156a7171c87d
Johan Stuyts <j.stuyts@javathinker.com>
2024-08-10 09:13:55 +0200

    Added typealiases. Added source code. Reduced indentation of markup.

--- src/examples/kotlin/kwmdsl/examples/EncodingPage.kt
+++ src/examples/kotlin/kwmdsl/examples/EncodingPage.kt
@@ -2,34 +2,40 @@ package kwmdsl.examples
 
 import com.squins.kwmdsl.*
 import com.squins.kwmdsl.component.IKotlinWicketMarkupProvider
+import kwmdsl.examples.dsl_convenience_base_classes.firstSourceCodeLink
 
 class EncodingPage : ExamplesConvenienceBasePage() {
     companion object : IKotlinWicketMarkupProvider {
-        override val noVariantMarkup = markup<EncodingPage> {
-            wicketExtend {
-                classH1("title") { text("Wicket and the Default JVM Encoding") }
-                classDiv("content") {
-                    p {
-                        text("If you did not see a happy face, system property ")
-                        code { text("file.encoding") }
-                        text(" is not set to ")
-                        code { text("UTF-8") }
-                        text(". Unicode characters in the markup will not be rendered properly.")
-                    }
+        override val noVariantMarkup = markup<S> {
+// @formatter:off
+wicketExtend {
+    classH1("title") { text("Wicket and the Default JVM Encoding") }
+    classDiv("content") {
+        p {
+            text("If you did not see a happy face, system property ")
+            code { text("file.encoding") }
+            text(" is not set to ")
+            code { text("UTF-8") }
+            text(". Unicode characters in the markup will not be rendered properly.")
+        }
 
-                    p {
-                        text("Wicket converts the markup strings to bytes and back before sending them to the browser. If the encoding is not set to UTF-8, this will result in data loss: question marks will appear instead of the intended characters. This applies to all Wicket applications, not just this example one showing how to use the DSL.")
-                    }
+        p {
+            text("Wicket converts the markup strings to bytes and back before sending them to the browser. If the encoding is not set to UTF-8, this will result in data loss: question marks will appear instead of the intended characters. This applies to all Wicket applications, not just this example one showing how to use the DSL.")
+        }
 
-                    p {
-                        text("You either have to add ")
-                        code { text("-Dfile.encoding=UTF-8") }
-                        text(" or remove ")
-                        code { text("-Dfile.encoding=COMPAT") }
-                        text(" from the JVM parameters.")
-                    }
-                }
-            }
+        p {
+            text("You either have to add ")
+            code { text("-Dfile.encoding=UTF-8") }
+            text(" or remove ")
+            code { text("-Dfile.encoding=COMPAT") }
+            text(" from the JVM parameters.")
+        }
+    }
+    firstSourceCodeLink(this@Companion)
+}
+// @formatter:on
         }
     }
 }
+
+private typealias S = EncodingPage
