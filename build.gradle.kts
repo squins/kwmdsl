8de16f7c6b32b14465363fafe5d37f0ab240f3cb
Johan Stuyts <j.stuyts@javathinker.com>
2024-09-27 17:02:41 +0200

    Version upgrades.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ version = "1-SNAPSHOT"
 
 tasks.wrapper {
     // https://gradle.org/releases/
-    gradleVersion = "8.10"
+    gradleVersion = "8.10.2"
     distributionType = Wrapper.DistributionType.ALL
 }
 
